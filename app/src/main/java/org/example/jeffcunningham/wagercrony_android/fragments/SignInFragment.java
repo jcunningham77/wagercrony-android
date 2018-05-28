66a3c3be1cabaa6afec7f3f5e3dd2197fd3d9a56
Jeff Cunningham <jcunningham77@gmail.com>
2018-05-28 09:17:05 -0400

    constraint layout

--- app/src/main/java/org/example/jeffcunningham/wagercrony_android/fragments/SignInFragment.java
+++ app/src/main/java/org/example/jeffcunningham/wagercrony_android/fragments/SignInFragment.java
@@ -6,12 +6,18 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import org.example.jeffcunningham.wagercrony_android.MainActivity;
 import org.example.jeffcunningham.wagercrony_android.R;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import util.Logger;
 
 /**
@@ -23,6 +29,14 @@ public class SignInFragment extends Fragment {
     @Inject
     Logger logger;
 
+
+
+
+    @BindView(R.id.emailEditText)
+    EditText emailEditText;
+    @BindView(R.id.passwordEditText)
+    EditText passwordEditText;
+
     private static final String TAG = "SignInFragment";
 
     @Nullable
@@ -30,6 +44,17 @@ public class SignInFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signin, container, false);
         ((MainActivity) getActivity()).component().inject(this);
+        ButterKnife.bind(this,view);
+
+        passwordEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    checkInput();
+                }
+            }
+        });
+
 
         logger.info(TAG, "onCreateView: initialized logger");
         
@@ -38,4 +63,45 @@ public class SignInFragment extends Fragment {
 
         
     }
+
+    public void checkInput() {
+
+        final String email = emailEditText.getText().toString();
+        if (!isValidEmail(email)) {
+            //Set error message for email field
+            emailEditText.setError("Invalid Email");
+        }
+
+        final String pass = passwordEditText.getText().toString();
+        if (!isValidPassword(pass)) {
+            //Set error message for password field
+            passwordEditText.setError("Password cannot be empty");
+        }
+
+        if(isValidEmail(email) && isValidPassword(pass))
+        {
+            logger.info(TAG, "checkInput: input is valid");
+        }
+
+    }
+
+    // validating email id
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    // validating password
+    private boolean isValidPassword(String pass) {
+        if (pass != null && pass.length() >= 4) {
+            return true;
+        }
+        return false;
+    }
+
+
 }
